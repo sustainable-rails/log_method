aadde9bc338f215d3015bc83ea2c772749db1b37
Dave Copeland <davec@naildrivin5.com>
2021-12-30 15:40:53 -0500

    allow an array of procs if more than one is needed

--- lib/log_method/config.rb
+++ lib/log_method/config.rb
@@ -1,6 +1,6 @@
 class LogMethod::Config
-  attr_accessor :after_log_proc,
-                :current_actor_id_label,
+  attr_reader   :after_log_procs
+  attr_accessor :current_actor_id_label,
                 :current_actor_proc,
                 :external_identifier_method,
                 :trace_id_proc
@@ -10,11 +10,15 @@ class LogMethod::Config
   end
 
   def reset!
-    @after_log_proc             = NO_OP
+    @after_log_procs            = []
     @current_actor_id_label     = "current_actor_id"
     @current_actor_proc         = NO_OP
     @external_identifier_method = nil
     @trace_id_proc              = NO_OP
+  end
+
+  def after_log_proc=(proc_or_array)
+    @after_log_procs = Array(proc_or_array)
   end
 
 private
