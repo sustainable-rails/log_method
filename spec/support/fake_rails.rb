fd4a451e9044d134a98b8998cb657af8f0f441f6
Dave Copeland <davec@naildrivin5.com>
2022-09-02 16:04:54 -0400

    only require this on platforms where it's needed

--- spec/support/fake_rails.rb
+++ spec/support/fake_rails.rb
@@ -1,5 +1,8 @@
 require "active_support/tagged_logging"
-require "active_support/isolated_execution_state"
+major,minor,_ = RUBY_VERSION.split(/\./).map(&:to_i)
+if (major > 2) || ((major == 2) && (minor > 6))
+  require "active_support/isolated_execution_state"
+end
 module Rails
   def self.logger
     ActiveSupport::TaggedLogging.new(Logger.new($stdout))
